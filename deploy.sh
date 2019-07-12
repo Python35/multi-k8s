3064fac3e0b191c2df00e4e4925fa596984a4fc3
Python35 <stev.hooker@gmail.com>
2019-07-12 14:47:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
-openssl aes-256-cbc -K $encrypted_0c35eebf403c_key -iv $encrypted_0c35eebf403c_iv -in service.json.enc -out service.json -d
-curl https://sdk.cloud.google.com | bash > /dev/null;
-source $HOME/google-cloud-sdk/path.bash.inc
-gcloud components update kubectl
-gcloud auth activate-service-account --key-file service.json
-gcloud config set project multi-k8s-246509
-gcloud config set compute/zone europe-west1-b
-gcloud container clusters get-credentials multi-cluster
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
 docker build -t $DOCKERID/multi-client:latest -t $DOCKERID/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t $DOCKERID/multi-server:latest -t $DOCKERID/multi-server:$GIT_SHA -f ./server/Dockerfile ./client
 docker build -t $DOCKERID/multi-worker:latest -t $DOCKERID/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./client
